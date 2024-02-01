7fd6e7e3878a0366f11faf68f3364381fede38a4
Amir <soltamir194@gmail.com>
2024-02-01 11:52:36 -0500

    upd

--- server/jwtAuthorization.go
+++ server/jwtAuthorization.go
@@ -15,58 +15,12 @@ import (
 	"golang.org/x/crypto/bcrypt"
 )
 
-const minPasswordLen = 6
 const expirationDurationSeconds = 60 * 60 * 24 * 15
 
 type JWT struct {
 	JWT_ID string `json:"jwt_id"`
 	IP     string `json:"ip"`
 	EXP    int64  `json:"exp"`
-}
-
-func (server *Server) Signup(ginContext *gin.Context, email string, password string) (*models.User, error) {
-
-	if len(password) < minPasswordLen {
-		return nil, fmt.Errorf("signup error: password must bigger than %v charachters", minPasswordLen)
-	}
-
-	hashedPassword, err := hashPassword(password, server.Env.PASSWORD_HASHING_SALT)
-	if err != nil {
-		return nil, err
-	}
-
-	user, err := server.DB.queries.CreateUser(context.Background(), models.CreateUserParams{Email: email, HashedPassword: string(hashedPassword)})
-	if err != nil {
-		return nil, fmt.Errorf("signup error: %w", err)
-	}
-
-	errJWT := setJWT(ginContext, &user, server.Env.JWE_SECRET_KEY)
-	if errJWT != nil {
-		return nil, errJWT
-	}
-
-	return &user, nil
-}
-
-func (server *Server) Login(ginContext *gin.Context, email string, password string) (*models.User, error) {
-
-	user, err := server.DB.queries.GetUserByEmail(context.Background(), email)
-	if err != nil {
-		return nil, fmt.Errorf("login error: Invalid email/password")
-	}
-
-	errCompare := compareToHashedPassword(&user, password, server.Env.PASSWORD_HASHING_SALT)
-	if errCompare != nil {
-		return nil, fmt.Errorf("login error: Invalid email/password")
-	}
-
-	errJWT := setJWT(ginContext, &user, server.Env.JWE_SECRET_KEY)
-	if errJWT != nil {
-		return nil, errJWT
-	}
-
-	return &user, nil
-
 }
 
 func (server *Server) ParseJWT(jwtEncrypted string) (*JWT, error) {
